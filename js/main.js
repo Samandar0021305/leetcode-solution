91d6c969c806c7f8c2ba4d8ffb96268c88946d74
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-17 22:13:59 +0500

    numIdenticalPairs

--- js/main.js
+++ js/main.js
@@ -540,3 +540,18 @@
 
 //     return -1
 // };
+
+
+// var numIdenticalPairs = function(nums) {
+//     let res = 0
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=0;j<nums.length;j++){
+//             if(nums[i] === nums[j] && i<j){
+//                 res++
+//             }
+//         }
+//     }
+//     return res
+// };
+
+// console.log(numIdenticalPairs([1,1,1,1]))
