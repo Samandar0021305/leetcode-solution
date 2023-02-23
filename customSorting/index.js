9ba15eea367e531a15967e993ddd66f6140cc521
Samandar0021305 <TSamandardev89@gmail.com>
2023-02-23 09:58:44 +0500

    custom sorting

---
+++ customSorting/index.js
@@ -0,0 +1,22 @@
+var customSortString = function(order, s) {
+    let arr = []
+    for(let i =0;i<order.length;i++){
+        for(let j=0;j<s.length;j++){
+            if(order[i] == s[j]){
+                arr.push(order[i])
+            }
+            if(!s.includes(order[i])){
+                continue
+            }
+        }
+    }
+    
+    for(let item of s){
+        if(!order.includes(item)){
+            arr = [...arr,item]
+        }
+    }
+    return arr.join("")
+};
+
+console.log(customSortString('cba','abcd'))
